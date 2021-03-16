fc06afd94dfd3117d249b36208bd00063112e434
slaurijssen <serve.laurijssen@fujifilm.com>
2021-03-16 07:40:59 +0100

    added user profile

--- app/models.py
+++ app/models.py
@@ -5,6 +5,8 @@ from flask_login import UserMixin, AnonymousUserMixin
 from . import db
 from . import login_manager
 
+import datetime
+
 class Permission:
     FOLLOW = 1
     COMMENT = 2
@@ -74,6 +76,11 @@ class User(db.Model, UserMixin):
     password_hash=db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -82,6 +89,11 @@ class User(db.Model, UserMixin):
                 self.role = Role.query.filter_by(name='Administrator').first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
+
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
+        db.session.commit()
 
     def can(self, perm):
         return self.role is not None and self.role.has_permission(perm)
