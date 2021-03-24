0257c6d54c5df194acc7bc20c36bdd30f5ce061b
laurijssen <lauserver@hotmail.com>
2021-03-24 06:37:53 +0000

    added avatar[4~

--- app/models.py
+++ app/models.py
@@ -83,14 +83,16 @@ class User(db.Model, UserMixin):
     about_me = db.Column(db.Text())
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+    avatar_hash = db.Column(db.String(32))
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
-        print('email ' + self.email)
+
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = self.gravatar_hash()
+            db.session.add(self)
+
         if self.role is None:
-
-            print('email ' + self.email)
-
             if self.email == current_app.config['GEOFRIENDS_ADMIN']:
                 self.role = Role.query.filter_by(name='Administrator').first()
             if self.role is None:
@@ -101,9 +103,17 @@ class User(db.Model, UserMixin):
         db.session.add(self)
         db.session.commit()
 
+    def gravatar_hash(self):
+        return hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+
     def gravatar(self, size=100, default='identicon', rating='g'):
-        url = 'https://secure.gravatar.com/avatar'
-        hash = hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+
+        hash = self.avatar_hash or self.gravatar_hash()
+        
         return '{}/{}?s={}&d={}&r={}'.format(url, hash, size, default, rating)
 
     def can(self, perm):
@@ -182,6 +192,7 @@ class User(db.Model, UserMixin):
         if self.query.filter_by(email=new_email).first() is not None:
             return False
         self.email = new_email
+        self.avatar_hash = self.gravatar_hash()
         db.session.add(self)
         return True
 
