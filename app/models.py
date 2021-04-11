b4b94799a333d2e1567b8198e5cf02aa598f255d
Laurijssen <lauserve@hotmail.com>
2021-04-11 14:23:50 +0200

    added token and password validation

--- app/models.py
+++ app/models.py
@@ -159,6 +159,22 @@ class User(db.Model, UserMixin):
         db.session.add(self)
         db.session.commit()
 
+    def generate_auth_token(self, expiration):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_in=expiration)
+
+        return s.dumps({'id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+
+        try:
+            data = s.loads(token)
+        except:
+            return None
+
+        return User.query.get(data['id'])
+
     def gravatar_hash(self):
         return hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
 
